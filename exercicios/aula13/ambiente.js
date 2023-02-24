5875fb633ecbf256f79836a1cfe7c3da150f05d9
Lukas Natanael <96913773+LukasNatanael@users.noreply.github.com>
2023-02-24 09:45:04 -0300

    iniciando os exercícios

---
+++ exercicios/aula13/ambiente.js
@@ -0,0 +1,20 @@
+var cont = 1
+
+// Estrutura while
+while (cont <= 6) {
+    console.log(`Passo ${cont}`)
+    cont++
+}
+
+cont = 1
+
+// Estrutura do while
+do {
+    console.log(`Cont ${cont}`)
+    cont++
+} while (cont <= 6)
+
+// Estrutura for
+for (c=1; c<=6; c++) {
+    console.log(`Teste ${c}`)
+}
---
+++ exercicios/aula14/ambiente.js
@@ -0,0 +1,6 @@
+// for (inicialização; teste lógico; incremento)
+console.log('Vai começar...')
+for (var c = 1; c<=6; c++) {
+    console.log(`Valor de c: ${c}`)
+}
+console.log('FIM')

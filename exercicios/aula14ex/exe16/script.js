fedf782f74cbaefe232cbcf745db66a045273dee
Lukas Natanael <96913773+LukasNatanael@users.noreply.github.com>
2023-02-28 08:06:40 -0300

    update

--- exercicios/aula14ex/exe16/script.js
+++ exercicios/aula14ex/exe16/script.js
@@ -1,1 +1,15 @@
-alert('Olá')
+// Criando variáveis
+var form_inicio = document.querySelector('input#inicio')
+var form_fim = document.querySelector('input#fim')
+var form_passo = document.querySelector('input#passo')
+var resultado = document.querySelector('div#resultado')
+
+// Iniciando o laço
+
+function contar() {
+    // alert(`início: ${form_inicio.value}; fim: ${form_fim.value}: passo: ${form_passo.value}`)
+
+    for (form_inicio.value++; form_inicio.value <= form_fim.value; form_passo.value) {
+        resultado.innerHTML = form_inicio.value
+    }
+}

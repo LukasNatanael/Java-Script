6934e5a79c5bce91bc5013277f6175f166ac1a3b
Lukas Natanael <96913773+LukasNatanael@users.noreply.github.com>
2023-05-09 09:25:39 -0300

    Update aula26.js

--- Curso JS - Edson Maia/js/aula26.js
+++ Curso JS - Edson Maia/js/aula26.js
@@ -1,58 +1,60 @@
 // Pegando elementos no DOM
 
-let citação00          = document.querySelector('.citacao00')
-let dataCitacao        = citacao00.querySelector('h2')
-let textoCitacao       = citacao00.querySelector('blockquote')
-let autoriaCitacao     = citacao00.querySelector('span')
+let citacao00 = document.querySelector('#citacao00')
+let citacaoData = citacao00.querySelector('h2')
+let citacaoTexto = citacao00.querySelector('blockquote')
+let citacaoAutoria = citacao00.querySelector('span')
 
 let aviso = document.querySelector('#aviso')
-let divBaixar = document.querySelector('#divBaixar')
+let baixar = document.querySelector('#divBaixar')
 
-// Criando MODAL para exibir mensagem de erro
-function abrirModal() {
+// Abri modal (mensagem caso o usuário não preencha os dados)
+function modal() {
     location.href = '#abrirModal'
 }
 
-// Função para baixar a imagem
+// Baixar mensagem
 function download() {
     html2canvas(citacao00).then(canvas => {
         document.body.appendChild(canvas)
     })
 
     let aviso2 = document.querySelector('#aviso2')
-    aviso2.textContent = "Clique com o botão direito e escolha Salvar imagem como..."
+    aviso2.textContent = 'Clique com o botão direito e escolha SALVAR IMAGEM COMO..'
 }
 
-// Função para criar a mensagem
+// Limpar INPUTS
+const btnLimpar = document.querySelector('#btnLimpar')
+btnLimpar.addEventListener('click', () => {
+    document.location.reload()
+    window.scroll(0, 0)
+})
+
+
+// Criar mensagem
 function criarMensagem(data, citacao, autoria) {
-    dataCitacao.textContent = data
-    textoCitacao.textContent = citacao
-    autoriaCitacao.textContent = autoria
+    citacaoData.textContent = data
+    citacaoTexto.textContent = citacao
+    citacaoAutoria.textContent = autoria
 
     download()
 }
 
-// Botão para limpar a tela
-document.querySelector('#btnLimpar').addEventListener('click', () => {
-    document.location.reload()
-    window.scroll(0,0)
-})
-
 // Criando a citação
 function criarCitacao() {
     let dataInput = document.querySelector('#data').value
-    let citacao   = document.querySelector('#citacao').value
-    let autoria   = document.querySelector('#autoria').value
+    let citacao = document.querySelector('#citacao').value
+    let autoria = document.querySelector('#autoria').value
 
     let data = new Date(dataInput)
-    let dataFormatada = data.toLocaleDateString('pr-BR', {timeZone: 'UTC'})
+    let dataFormatada = data.toLocaleDateString('pt-BR', {timeZone: 'UTC'})
 
-    // Validação simples
-    if (data == '' || citacao == '' || autoria == '') {
-        abrirModal()
+    // Verificação simples
+    if(dataInput == '' || citacao == '' || autoria == '') {
+        modal()
     }
     else {
         criarMensagem(dataFormatada, citacao, autoria)
-        window.scroll(0, 670)
+        window.scroll(0,670)
     }
 }

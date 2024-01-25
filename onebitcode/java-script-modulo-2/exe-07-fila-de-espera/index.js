89d5d5ed28ae59509cb084f5ba4e33d4c19a2d7c
Lukas Natanael <lukasnatanael889@gmail.com>
2024-01-25 17:42:32 -0300

    Update index.js

--- onebitcode/java-script-modulo-2/exe-07-fila-de-espera/index.js
+++ onebitcode/java-script-modulo-2/exe-07-fila-de-espera/index.js
@@ -1,48 +1,82 @@
-let pacients = ['Lukas', 'Matheus']
-let pacientsMenu = ''
-// pacients.length == 0 ? console.log('Ainda não possui nenhum paciente na fila') : pacients.forEach((pacient, position) => console.log(`${position} - ${pacient}`))
+// let pacients = []
+let pacients = []
+let pacientsList = ''
+
+// pacients.length > 0 ? pacients.forEach( (pacient, position) =>  { console.log(position, pacient); console.log() } ): alert('A lista não possui nenhum paciente')
 
 
-pacients.length > 0 ? pacients.forEach( (pacient, position) =>  { console.log(position, pacient); console.log() } ): alert('A lista não possui nenhum paciente')
-
-
-// if (pacients.length > 0) {
-//     pacients.forEach((pacient, position) => {
-//         pacientsMenu += `${position+1} - ${pacient}\n`
-//     })
-//     alert(`Fila de pacientes: \n${pacientsMenu}`)
-// }
+if (pacients.length > 0) {
+    console.log('Lista de pacientes:')
+    pacients.forEach( (pacient, position) =>  { 
+        console.log(`${position+1}º - ${pacient}`)
+        }
+    )
+}
 
 
 let option = ''
 let newPacient = ''
 
 do {
-    option = parseFloat(prompt('Menu interativo do consultório: \n1 - Novo paciente \n2 - Consultar paciente \n3 - Sair'))
+    option = parseFloat(prompt('Menu interativo do consultório: \n1 - Novo paciente \n2 - Consultar paciente \n3 - Listar pacientes \n4 - Sair'))
     switch (option) {
         case 1:
             newPacient = prompt('Qual o nome do paciente: ')
             pacients.push(newPacient)
-            alert(`Paciente ${newPacient} foi adicionado a ${pacients.indexOf(newPacient)+1}ª posição da fila. \n${pacients}`)
-            
-            pacientsMenu += `${newPacient} \n`
+            pacientsList = ''
             console.clear()
-            console.log(pacientsMenu)
-            // alert(pacients)
-            
+            console.log(`Paciente ${newPacient} foi adicionado a ${pacients.length}ª posição da fila. \n`)
+
+            pacients.forEach( (pacient, position) =>  { 
+                console.log(`${position+1}º - ${pacient}`)
+                pacientsList += `${position+1}º - ${pacient} \n`
+                }
+            )
+
+            alert(`Paciente ${newPacient} foi adicionado a ${pacients.indexOf(newPacient)+1}ª posição da fila. \n${pacientsList}`)
+            console.log()
+            // console.log(pacientsList)
             break
+
         case 2:
             if (pacients.length >=1) {
                 let removedPacient = pacients.shift()
-                alert(`Paciente ${removedPacient} recebeu alta.`)
+                pacientsList = ''
+
+                console.clear()
+                console.log(`Paciente ${removedPacient} recebeu alta.`)
+                pacients.forEach( (pacient, position) =>  { 
+                    console.log(`${position+1}º - ${pacient}`)
+                    pacientsList += `${position+1}º - ${pacient} \n`
+                    }
+                )
+
+                alert(`Paciente ${removedPacient} recebeu alta. \n${pacientsList}`)
             }
             else {
+                console.clear()
+                console.log('A lista de pacientes está vazia!')
                 alert('A lista de pacientes está vazia!')
             }
             break
         
         case 3:
+            console.clear()
+            if (pacients.length > 0) {
+
+                console.log(`Lista de pacientes: \n${pacientsList}`)
+                alert(`Lista de pacientes: \n${pacientsList}`)
+            }
+            else {
+                console.log('Não possui nenhum paciente na fila de espera.')
+                alert('Não possui nenhum paciente na fila de espera.')
+            }
+            break
+
+        case 4:
            alert('Finalizando consultas...')
            break
     }
-} while(option != 3)
+} while(option != 4)
+
+alert('As consultas de hoje estão todas finalizadas.')

a9ffa56211f331cb9bb28621b388dab5e82459d8
Lukas Natanael <lukasnatanael889@gmail.com>
2024-01-26 17:11:03 -0300

    update exercicies

--- onebitcode/java-script-modulo-2/exe-07-fila-de-espera/index.js
+++ onebitcode/java-script-modulo-2/exe-07-fila-de-espera/index.js
@@ -1,82 +1,40 @@
-// let pacients = []
+// let pacients = ['Lukas', 'Matheus', 'João']
 let pacients = []
 let pacientsList = ''
 
-// pacients.length > 0 ? pacients.forEach( (pacient, position) =>  { console.log(position, pacient); console.log() } ): alert('A lista não possui nenhum paciente')
-
-
-if (pacients.length > 0) {
-    console.log('Lista de pacientes:')
-    pacients.forEach( (pacient, position) =>  { 
-        console.log(`${position+1}º - ${pacient}`)
-        }
-    )
-}
-
-
 let option = ''
-let newPacient = ''
 
 do {
-    option = parseFloat(prompt('Menu interativo do consultório: \n1 - Novo paciente \n2 - Consultar paciente \n3 - Listar pacientes \n4 - Sair'))
+    pacientsList = ''
+    pacients.forEach( (pacient, position) =>  { 
+        pacientsList += `${position+1}º - ${pacient} \n`
+        }
+    )
+
+    option = parseFloat(prompt(`Pacientes: \n${pacientsList} \nEscolha uma ação: \n1 - Novo paciente \n2 - Consultar paciente \n3 - Sair`))
     switch (option) {
         case 1:
-            newPacient = prompt('Qual o nome do paciente: ')
+            let newPacient = prompt('Qual o nome do paciente: ')
             pacients.push(newPacient)
-            pacientsList = ''
+
             console.clear()
-            console.log(`Paciente ${newPacient} foi adicionado a ${pacients.length}ª posição da fila. \n`)
-
-            pacients.forEach( (pacient, position) =>  { 
-                console.log(`${position+1}º - ${pacient}`)
-                pacientsList += `${position+1}º - ${pacient} \n`
-                }
-            )
-
-            alert(`Paciente ${newPacient} foi adicionado a ${pacients.indexOf(newPacient)+1}ª posição da fila. \n${pacientsList}`)
-            console.log()
-            // console.log(pacientsList)
+            alert(`Paciente ${newPacient} foi adicionado a ${pacients.indexOf(newPacient)+1}ª posição da fila.`)
             break
 
         case 2:
-            if (pacients.length >=1) {
+            if (pacients.length > 0) {
                 let removedPacient = pacients.shift()
-                pacientsList = ''
-
-                console.clear()
-                console.log(`Paciente ${removedPacient} recebeu alta.`)
-                pacients.forEach( (pacient, position) =>  { 
-                    console.log(`${position+1}º - ${pacient}`)
-                    pacientsList += `${position+1}º - ${pacient} \n`
-                    }
-                )
-
-                alert(`Paciente ${removedPacient} recebeu alta. \n${pacientsList}`)
+                alert(`Paciente ${removedPacient} recebeu alta.`)
             }
             else {
-                console.clear()
-                console.log('A lista de pacientes está vazia!')
                 alert('A lista de pacientes está vazia!')
             }
             break
         
         case 3:
-            console.clear()
-            if (pacients.length > 0) {
-
-                console.log(`Lista de pacientes: \n${pacientsList}`)
-                alert(`Lista de pacientes: \n${pacientsList}`)
-            }
-            else {
-                console.log('Não possui nenhum paciente na fila de espera.')
-                alert('Não possui nenhum paciente na fila de espera.')
-            }
-            break
-
-        case 4:
            alert('Finalizando consultas...')
            break
     }
-} while(option != 4)
+} while(option != 3)
 
 alert('As consultas de hoje estão todas finalizadas.')
--- onebitcode/java-script-modulo-2/exe-08-pilha-de-cartas/index.js
+++ onebitcode/java-script-modulo-2/exe-08-pilha-de-cartas/index.js
@@ -1,16 +1,21 @@
 let cards = []
 let option = ''
 
-alert(`Atualmente possuem: ${cards.length} cartas no baralho`)
 do {
-    option = parseFloat(prompt('Menu interativo do consultório: \n1 - Adicionar carta \n2 - Remover carta \n3 - Sair'))
+    option = parseFloat(prompt(`Cartas no baralho: ${cards.length}\n \nMenu interativo: \n1 - Adicionar carta \n2 - Remover carta \n3 - Sair`))
 
     switch(option) {
         case 1:
-            alert('Você escolheu a opção número 1')
+            console.clear()
+            cards.unshift(prompt('Qual carta você deseja adicionar ao baralho: '))
             break
         case 2:
-            alert('Você escolheu a opção número 2')
+            if (cards.length > 0) {
+                alert(`A carta retirada do baralho foi: ${cards.shift()}`)
+            }
+            else {
+                alert('Não possuem mais cartas no baralho!')
+            }
             break
         case 3:
             alert('Finalizando programa...')
@@ -20,3 +25,4 @@ do {
 } while(option != 3)
 
 alert('Programa finalizado!')
+alert(`Atualmente possuem: ${cards.length} cartas no baralho`)

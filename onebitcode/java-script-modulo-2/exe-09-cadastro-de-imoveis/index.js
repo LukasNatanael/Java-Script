57fd54cf807a8e72b703f88bbdd59fe2c22f1f89
Lukas Natanael <96913773+LukasNatanael@users.noreply.github.com>
2024-01-31 07:56:59 -0300

    Update index.js

--- onebitcode/java-script-modulo-2/exe-09-cadastro-de-imoveis/index.js
+++ onebitcode/java-script-modulo-2/exe-09-cadastro-de-imoveis/index.js
@@ -1,41 +1,60 @@
 let properties = []
 let option = ''
 
-
 do {
+    let propertiesList = ''
     option = parseFloat(prompt(
-        'Imovéis cadastrados: ' + properties.length +
-        '\nEscolha uma opção:' +
+        'Bem vindo(a) ao Cadastro de Imóveis' +
+        '\nTotal de imovéis: ' + properties.length +
+        '\n\nEscolha uma opção:' +
         '\n1 - Salvar imóvel' +
         '\n2 - Mostrar imóveis' +
         '\n3 - Encerrar'
     ))
 
     switch(option) {
+        
         case 1:
             const data = {}
 
-            data.ownerName = prompt('Nome do proprietário: ')
-            data.roomsNumber = prompt('Quantidade de quartos: ')
-            data.bathsNumber = prompt('Quantidade de banheiros: ')
-            data.garage = prompt('Possui garagem [S/N]: ')[0].toLowerCase()
+            data.ownerName = prompt('Nome do proprietário do imóvel: ')
+            data.roomsNumber = prompt('Quantos quartos o imóvel possui: ')
+            data.bathsNumber = prompt('Quantidade de banheiros do imóvel: ')
+            data.garage = prompt('Imóvel possui garagem [S/N]: ')[0].toUpperCase()
 
-            const confirm = prompt(
-                'Proprietário: ' + data.ownerName +
+            const confirmation = confirm(
+                'Salvar esse imóvel ?\n' +
+                '\nProprietário: ' + data.ownerName +
                 '\nQuantidade de quartos: ' + data.roomsNumber +
                 '\nQuantidade de banheiros: ' + data.roomsNumber +
-                '\nPossui garagem: ' + data.garage +
-                '\nVocê deseja salvar esse imóvel ? [S/N] ')[0].toLowerCase()
+                '\nPossui garagem: ' + data.garage
+            )
 
-
-            properties.push(data)
-
+            if (confirmation == true) {
+                properties.push(data)
+                alert('Imóvel adicionado a lista de propriedades')
+            }
+            else {
+                alert('Imóvel descartado. Voltando ao menu.')
+            }
             break
+
         case 2:
+            propertiesList = ''
+            properties.forEach( (element, pos) => {
+                alert(`\n${pos+1}º IMÓVEL \nProprietário: ${element.ownerName}\nQuantidade de quartos: ${element.roomsNumber}\nQuantidade de banheiros: ${element.bathsNumber}\nPossui garagem: ${element.garage}\n`)
+                }
+            )
+            // alert(`Lista de imóveis: \n${propertiesList}`)
             break
+        
         case 3:
             alert('Encerrando programa...')
             break
+            
+        default:
+            alert('Opção inválida!')
+        
         }
         
 } while (option != 3)

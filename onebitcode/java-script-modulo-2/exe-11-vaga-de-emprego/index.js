d1717e3c673ff9a5e4c8ec0bf023710281462313
Lukas Natanael <96913773+LukasNatanael@users.noreply.github.com>
2024-02-12 08:19:37 -0300

    Update index.js

--- onebitcode/java-script-modulo-2/exe-11-vaga-de-emprego/index.js
+++ onebitcode/java-script-modulo-2/exe-11-vaga-de-emprego/index.js
@@ -1,6 +1,6 @@
-let vacancies = []
+vagas = []
 
-function newVacancy() {
+function novaVaga() {
     name = prompt('Informe um nome para a vaga: ')
     description = prompt('Informe a descrição da vaga: ')
     deadline = prompt('Informe uma data limite (dd/mm/aaaa) para inscrição: ')
@@ -17,14 +17,83 @@ function newVacancy() {
             name,
             description,
             deadline,
-            []
+            subscribers: []
         }
+        vagas.push(newVacancy)
+        alert('Vaga criada!')
     }
 
 }
 
+function exibirVaga() {
+    const index = prompt('Informe o índice da vaga que deseja exibir: ')
+    const vaga = vagas[index]
+
+    const candidatosEmTexto = vagas.subscribers.reduce( (textoFinal, candidato, indice) => {
+        // return textoFinal +`\n${indice}. ${candidato}`
+        return textoFinal +`\n - ${candidato}`
+    }, '' )
+    alert(
+        `Vaga nº ${indice}` +
+        `Nome: ${vaga.name}` +
+        `Descrição: ${vaga.description}` +
+        `Data limite: ${vaga.deadline}` +
+        `Quantidade de candidatos: ${vaga.subscribers.length}` +
+        `Candidatos inscritos: ${candidatosEmTexto}`
+    )
+}
+
+
+function listarVagas() {
+    const vagasEmTexto = vagas.reduce( (textoFinal, vaga, indice) => {
+        textoFinal += `${indice}. `
+        textoFinal += vaga.name
+        textoFinal += `(${vaga.subscribers.length} candidatos) \n`
+        return textoFinal
+    }, '')
+
+    alert(vagasEmTexto)
+}
+
+function registerCandidate() {
+    const candidate = prompt('Informe o nome do(a) candidato(a): ')
+    const index = prompt('Informe o índice da vaga para qual o(a) candidato(a) deseja se inscrever: ')
+    const vaga = vaga[index]
+
+    const confirmation = confirm(
+        `Deseja inscrever o candidato ${candidate} na vaga ${vaga} ? \n` +
+        `Nome: ${vaga.nome} \n` +
+        `Descrição: ${vaga.description} \n` +
+        `Data limite: ${vaga.deadline} \n`
+
+    )
+
+    if (confirmation) {
+        vaga.subscribers.push(candidate)
+        alert('Inscrição realizada!')
+    }
+}
+
+function removeVacancy() {
+    const index = prompt('Informe o índice da vaga que deseja excluir: ')
+    const vaga = vaga[index]
+
+    const confirmation = confirm(
+        `Tem certeza de que deseja excluir a vaga ${vaga} ? \n` +
+        `Nome: ${vaga.nome} \n` +
+        `Descrição: ${vaga.description} \n` +
+        `Data limite: ${vaga.deadline} \n`
+    )
+
+    if (confirmation) {
+        vaga.splice(index, 1)
+        alert('Vaga removida!')
+    }
+}
+
+
 function showMenu() {
-    return parseFloat(prompt(
+    const opção = parseFloat(prompt(
         'Bem vindo(a) ao Sistema de Vagas de Emprego\n\n' +
         'Escolha uma opção: \n' +
         '1 - Listar vagas disponíveis \n' +
@@ -35,6 +104,8 @@ function showMenu() {
         '6 - Sair \n'
     
         ))
+
+    return opção
 }
 
 
@@ -63,3 +134,5 @@ function execute() {
         } while (option != 6)
         alert('Programa finalizado!')
 }
+
+// execute()

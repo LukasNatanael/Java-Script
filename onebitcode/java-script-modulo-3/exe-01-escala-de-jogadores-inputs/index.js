1e0934a58f12e96075b2503efeb0b5e315d9473e
Lukas Natanael <96913773+LukasNatanael@users.noreply.github.com>
2024-02-29 10:13:59 -0300

    update

--- onebitcode/java-script-modulo-3/exe-01-escala-de-jogadores-inputs/index.js
+++ onebitcode/java-script-modulo-3/exe-01-escala-de-jogadores-inputs/index.js
@@ -83,27 +83,44 @@ ul.appendChild(climbButton)
 // sectionRegister.append(h3, ul)
 sectionRegister.append(ul)
 
+const sectionListTop = document.querySelector('#sectionListTop')
 
+// Criando evento para remover jogador atual 
+const removeSection = document.createElement('section')
+const removeButton  = document.createElement('button')
+const removeInput   = document.createElement('input')
+
+removeSection.id = 'remove-section'
+
+removeInput.placeholder = 'Número da camisa do jogador a ser removido'
+removeInput.id = 'remove-input-number'
+
+removeButton.innerText = 'Remover jogador'
+removeButton.id = 'remove-input-button'
+
+removeSection.append(removeInput, removeButton)
+
+sectionListTop.appendChild(removeSection)
 
 climbButton.addEventListener( 'click', () => {
-    const inputPlayerName = document.querySelector('#name')
+    const inputPlayerName     = document.querySelector('#name')
     const inputPlayerPosition = document.querySelector('#position')
-    const inputPlayerNumber = document.querySelector('#number')
+    const inputPlayerNumber   = document.querySelector('#number')
 
-    const sectionList = document.querySelector('#sectionList')
-    const playerList  = document.createElement('ul')
-    const h3          = document.createElement('h3')
+    const sectionListBottom = document.querySelector('#sectionListBottom')
+    const playerList        = document.createElement('ul')
+    const h3                = document.createElement('h3')
 
-    const nameLi      = document.createElement('li')
-    const positionLi  = document.createElement('li')
-    const numberLi    = document.createElement('li')
+    const nameLi     = document.createElement('li')
+    const positionLi = document.createElement('li')
+    const numberLi   = document.createElement('li')
 
     playerList.style = 'list-style-type: none;'
-
+    
     // Adicionando jogador a lista de jogadores
-    const playerName = inputPlayerName.value
+    const playerName     = inputPlayerName.value
     const playerPosition = inputPlayerPosition.value
-    const playerNumber = inputPlayerNumber.value
+    const playerNumber   = inputPlayerNumber.value
 
     if (playerName.length == 0 || playerPosition.length == 0 || playerNumber.length == 0) {
         alert('Por favor preencha os dados do jogador!')
@@ -128,12 +145,14 @@ climbButton.addEventListener( 'click', () => {
             numberLi.id = `${playerName.toLowerCase()}-number`
 
             // playerList.append(nameLi, positionLi, numberLi)
+            // playerList.append(h3, nameLi, positionLi, numberLi, removeButton)
+
             playerList.append(h3, nameLi, positionLi, numberLi)
-            playerList.id = `player-list-${playerName.toLowerCase()}`
+            playerList.id = `player-list-${playerNumber}`
 
             h3.innerText = `Dados do jogador ${playerName}`
             // sectionList.append(h3, playerList)
-            sectionList.append(playerList)
+            sectionListBottom.append(playerList)
         }
         // Limpando inputs 
         inputPlayerName.value = ''
@@ -141,16 +160,25 @@ climbButton.addEventListener( 'click', () => {
         inputPlayerNumber.value = ''
     }
 
+    removeButton.addEventListener( 'click', () => {
+            const playerToRemove = document.querySelector(`player-list-${removeInput.value}`)
+
+            const confirmation = confirm(
+                `Deseja remover o jogador ${playerName} com os dados: \n\n` +
+                `Nome: ${playerName}\n` +
+                `Posição: ${playerPosition}\n` +
+                `N° camisa: ${playerNumber}\n`
+            )
+            if (confirmation) {
+                playerList.remove(playerToRemove)
+                alert('O jogador agora não faz mais parte do time!')
+            }
+            else {
+                alert('Falha ao remover jogador do time!')
+            }
+
+            removeInput.value = ''
+        }
+    )
 
 } )
-
-function removePlayer() {
-    
-    const sectionRegister = document.querySelector('#sectionRegister')
-    const title       = document.querySelectorAll('h3')
-    const playersList = document.querySelectorAll('ul')
-
-    sectionRegister.removeChild(title[title.length - 1])
-    sectionRegister.removeChild(playersList[playersList.length - 1])
-
-}

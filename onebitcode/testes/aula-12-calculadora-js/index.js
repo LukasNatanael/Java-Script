e9f34f7ddaa3f9ed7095dab47a0d7b3d8923362f
Lukas Natanael <96913773+LukasNatanael@users.noreply.github.com>
2024-04-01 08:16:47 -0300

    Update index.js

--- onebitcode/testes/aula-12-calculadora-js/index.js
+++ onebitcode/testes/aula-12-calculadora-js/index.js
@@ -29,49 +29,102 @@ Verificar se expressão é válida para calcular
 
 */
 
-const main = document.querySelector('main')
-const root = document.querySelector(':root') // :root do CSS
-const input = document.querySelector('#input')
+// selecionando elementos necessários
+const main   = document.querySelector('main')
+const root   = document.querySelector(':root')
+const input  = document.querySelector('#input')
 const resultInput = document.querySelector('#result')
 
-// definindo teclas que podem ser pressionadas
+// definindo teclas permitidas
 const allowedKeys = ["(", ")", "/", "*", "-", "+", "9", "8", "7", "6", "5", "4", "3", "2", "1", "0", ".", "%", " "]
 
-// Verificando botões que foram PRESSIONADOS
-document.querySelectorAll('.charKey').forEach( charkeyBtn => {
-    // verificando qual tecla foi pressionada
-    charkeyBtn.addEventListener('click', () => {
-        const value = charkeyBtn.dataset.value
+// verificando teclas que foram PRESSIONADAS, selecionando por classe
+document.querySelectorAll('.charKey').forEach( charKeyBtn => {
+    charKeyBtn.addEventListener('click', () => {
+        const value = charKeyBtn.dataset.value
         input.value += value
-        return // finalizando função
+        return // finalizando função sem retorno algum
     })
-} )
+})
 
-// Verificando botões que foram DIGITADOS
+// limpar input
+document.querySelector('#clear').addEventListener('click', () => {
+    input.value = ''
+    input.focus()
+})
+
+document.querySelector('#equal').addEventListener('click', () => {
+    calculate()
+})
+
+// verificando e validando teclas DIGITADAS
 document.addEventListener('keydown', (event) => {
     event.preventDefault()
-    
     if (allowedKeys.includes(event.key)) {
         input.value += event.key
+        return // finalizando função sem retorno algum
     }
 
-    if (charkeyBtn.key === 'Backspace') {
+    if (event.key === 'Backspace') {
         input.value = input.value.slice(0, -1)
-        return // finalizando função
+        return // finalizando função sem retorno algum
     }
-    
-    if (charkeyBtn.key === 'Enter') {
-        calculate()
-    }
-    
-    if (charkeyBtn.key === 'Escape') {
+
+    if (event.key === 'Escape') {
         input.value = ''
         input.focus()
     }
 
+    if (event.key === 'Enter') {
+        calculate()
+    }
 })
 
+// validando e calculando valores do input
 function calculate() {
-    const result = eval(input.value)
-    resultInput.value = result
+    resultInput.value = 'ERROR'
+    resultInput.classList.add('error')
+    
+    if (input.value == '') {
+        resultInput.value = 'ERROR'
+        resultInput.classList.add('error')
+        return
+    }
+    else {
+        const result = eval(input.value)
+        resultInput.value = result
+        resultInput.classList.remove('error')
+    }
 }
+// copiando resultado para a área de transferência
+document.querySelector('#copyToClipboard').addEventListener('click', (event) => {
+    const button = event.currentTarget
+    if (button.innerText == 'Copy') {
+        button.innerText = 'Copied!'
+        button.classList.add('success')
+        navigator.clipboard.writeText(resultInput.value)
+    }
+    else {
+        button.innerText = 'Copy'
+        button.classList.remove('success')
+    }
+})
+
+// trocando o tema da página
+document.querySelector('#themeSwitcher').addEventListener('click', () => {
+    if (main.dataset.theme === 'dark') {
+        root.style.setProperty('--bg-color',      '#f1f5f9')
+        root.style.setProperty('--border-color',  '#aaa')
+        root.style.setProperty('--font-color',    '#212529')
+        root.style.setProperty('--primary-color', '#26834a')
+        main.dataset.theme = 'light'
+    }
+    else {
+        root.style.setProperty('--bg-color',      '#212529')
+        root.style.setProperty('--border-color',  '#666')
+        root.style.setProperty('--font-color',    '#f1f5f9')
+        root.style.setProperty('--primary-color', '#4dff91')
+        main.dataset.theme = 'dark'
+
+    }
+})

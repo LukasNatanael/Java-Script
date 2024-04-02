67dfecac95cfc049db811b5582f6fc3dc07caf60
Lukas Natanael <lukasnatanael889@gmail.com>
2024-04-02 17:16:45 -0300

    update exercise

--- onebitcode/java-script-modulo-3/exe-03-tic-tac-toe/index.js
+++ onebitcode/java-script-modulo-3/exe-03-tic-tac-toe/index.js
@@ -1,2 +1,45 @@
-document.querySelector('#player-one').innerText = '15'
-document.querySelector('#player-two').innerText = '7'
+const button = document.querySelectorAll('button')
+
+const allowedKeys = [ '1', '2', '3', '4', '5', '6', '7', '8', '9']
+
+document.addEventListener('keydown', (event) => {
+    const value = event.key
+    if (allowedKeys.includes(value)) {
+        console.log(value)
+    }
+
+})
+
+let cont = 0
+
+document.querySelectorAll('button').forEach( element => {
+    element.addEventListener('click', (event) => {
+        event.preventDefault()
+        // event.currentTarget.style.backgroundColor = 'red'
+        // event.currentTarget.innerText = 'red'
+
+        if (cont === 8) {
+            alert('Fim de jogo')
+        }
+        else {
+            cont += 1
+        }
+
+        if (cont % 2 === 0) {
+            event.currentTarget.innerHTML = '<i class="icons fa-solid fa-x"></i>'
+            element.disabled = true
+        }
+        else {
+            event.currentTarget.innerHTML = '<i class="icons fa-regular fa-circle"></i>'
+            element.disabled = true
+            
+        }
+        
+    })
+})
+
+
+document.querySelector('#play-again').addEventListener('click', () => {
+    button.forEach( element => element.innerHTML = ''  )
+    cont = 0
+})

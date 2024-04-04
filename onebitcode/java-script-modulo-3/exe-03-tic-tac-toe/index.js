d4fd3861de5aa964ff637e6db25110f2d609216c
Lukas Natanael <96913773+LukasNatanael@users.noreply.github.com>
2024-04-04 08:09:21 -0300

    update

--- onebitcode/java-script-modulo-3/exe-03-tic-tac-toe/index.js
+++ onebitcode/java-script-modulo-3/exe-03-tic-tac-toe/index.js
@@ -1,5 +1,6 @@
 const button = document.querySelectorAll('button')
-let cont = 0
+let clickCont = 0
+let pressCont = 0
 let botao 
 const allowedKeys = [ '1', '2', '3', '4', '5', '6', '7', '8', '9']
 
@@ -18,22 +19,30 @@ document.addEventListener('keydown', (event) => {
             
             // cont = 0
             if (allowedKeys.includes(event.key)) {
-                if (cont % 2 === 0) {
-                    playerTwo.classList.add('player-round')
-                    playerOne.classList.remove('player-round')
+                if (pressCont % 2 === 0) {
+                    // playerTwo.classList.add('player-round')
+                    // playerOne.classList.remove('player-round')
                     botao = '<i class="icons fa-solid fa-x"></i>'
+                    element.dataset.key = 'x'
+                    
                 }
                 else {
-                    playerOne.classList.add('player-round')
-                    playerTwo.classList.remove('player-round')
+                    // playerOne.classList.add('player-round')
+                    // playerTwo.classList.remove('player-round')
                     botao = '<i class="icons fa-regular fa-circle"></i>'
+                    element.dataset.key = 'O'
+
                 }
 
-                value.innerHTML = botao
-                console.log(cont)
             }
+            pressCont += 1
+            // console.log(`c: ${pressCont}`)
+            if (element.disabled != true) {
+                value.innerHTML = botao
+                element.disabled = true
+            }
+
         }
-        cont += 1
     })
 
 })
@@ -44,11 +53,12 @@ document.querySelectorAll('button').forEach( element => {
     element.addEventListener('click', (event) => {
         event.preventDefault()
 
-        if (cont % 2 === 0) {
+        if (pressCont % 2 === 0) {
             playerTwo.classList.add('player-round')
             playerOne.classList.remove('player-round')
             event.currentTarget.innerHTML = '<i class="icons fa-solid fa-x"></i>'
             botao = '<i class="icons fa-solid fa-x"></i>'
+            element.dataset.key = 'x'
             element.disabled = true
         }
         else {
@@ -56,18 +66,19 @@ document.querySelectorAll('button').forEach( element => {
             playerTwo.classList.remove('player-round')
             event.currentTarget.innerHTML = '<i class="icons fa-regular fa-circle"></i>'
             botao = '<i class="icons fa-regular fa-circle"></i>'
+            element.dataset.key = 'O'
             element.disabled = true
             
         }
 
-        cont += 1
+        pressCont += 1
         
     })
 })
 
 
 document.querySelector('#play-again').addEventListener('click', () => {
-    cont = 0
+    clickCont = 0
     const numbers = [
         7, 4, 1,
         8, 5, 2,
@@ -75,23 +86,23 @@ document.querySelector('#play-again').addEventListener('click', () => {
     ]
 
     button.forEach( element => {
-        element.innerHTML = numbers[cont]
+        element.innerHTML = numbers[clickCont]
         element.disabled = false
         playerOne.classList.add('player-round')
         playerTwo.classList.remove('player-round')
-        if (cont == 8) {
-            cont = 0
+        if (clickCont == 8) {
+            clickCont = 0
             
         }
         else {
-            cont += 1
+            clickCont += 1
         }
     })
 })
 
 document.addEventListener('keydown', (event) => {
     event.preventDefault()
-    cont = 0
+    clickCont = 0
     const numbers = [
         7, 4, 1,
         8, 5, 2,
@@ -101,16 +112,16 @@ document.addEventListener('keydown', (event) => {
     if (event.key === 'Escape') {
         
         button.forEach( element => {
-            element.innerHTML = numbers[cont]
+            element.innerHTML = numbers[clickCont]
             element.disabled = false
             playerOne.classList.add('player-round')
             playerTwo.classList.remove('player-round')
-            if (cont == 8) {
-                cont = 0
+            if (clickCont == 8) {
+                clickCont = 0
                 
             }
             else {
-                cont += 1
+                clickCont += 1
             }
         })
     }

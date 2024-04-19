eceb7cfe1db498f8d98da825325d0ead2f2bd588
Lukas Natanael <96913773+LukasNatanael@users.noreply.github.com>
2024-04-19 07:40:00 -0300

    finish game

--- onebitcode/testes/exe-03-tic-tac-toe/index.js
+++ onebitcode/testes/exe-03-tic-tac-toe/index.js
@@ -15,13 +15,51 @@ function initializeGame() {
     boardRegions.forEach( element => {
         element.classList.remove('win')
         element.innerText = ''
+        element.classList.add('cursor-pointer')
         element.addEventListener('click', handleBoardClick)
     })
 }
 
+function getWinRegions() {
+    const winRegions = []
+    if (virtualBoard[0][0] && virtualBoard[0][0] === virtualBoard[0][1] && virtualBoard[0][0] === virtualBoard[0][2])
+    winRegions.push("0.0", "0.1", "0.2")
+
+    if (virtualBoard[1][0] && virtualBoard[1][0] === virtualBoard[1][1] && virtualBoard[1][0] === virtualBoard[1][2])
+    winRegions.push("1.0", "1.1", "1.2")
+
+    if (virtualBoard[2][0] && virtualBoard[2][0] === virtualBoard[2][1] && virtualBoard[2][0] === virtualBoard[2][2])
+    winRegions.push("2.0", "2.1", "2.2")
+
+    if (virtualBoard[0][0] && virtualBoard[0][0] === virtualBoard[1][0] && virtualBoard[0][0] === virtualBoard[2][0])
+    winRegions.push("0.0", "1.0", "2.0")
+
+    if (virtualBoard[0][1] && virtualBoard[0][1] === virtualBoard[1][1] && virtualBoard[0][1] === virtualBoard[2][1])
+    winRegions.push("0.1", "1.1", "2.1")
+
+    if (virtualBoard[0][2] && virtualBoard[0][2] === virtualBoard[1][2] && virtualBoard[0][2] === virtualBoard[2][2])
+    winRegions.push("0.2", "1.2", "2.2")
+
+    if (virtualBoard[0][0] && virtualBoard[0][0] === virtualBoard[1][1] && virtualBoard[0][0] === virtualBoard[2][2])
+    winRegions.push("0.0", "1.1", "2.2")
+
+    if (virtualBoard[0][2] && virtualBoard[0][2] === virtualBoard[1][1] && virtualBoard[0][2] === virtualBoard[2][0])
+    winRegions.push("0.2", "1.1", "2.0")
+
+    return winRegions
+}
+
 function disableRegion(element) {
-    element.style.cursor = 'default'
+    element.classList.remove('cursor-pointer')
     element.removeEventListener('click', handleBoardClick)
+}
+
+function handleWin(regions) {
+    regions.forEach(region => {
+        document.querySelector(`[data-region="${region}"]`).classList.add('win')
+    })
+    const playerName = document.querySelector(`#${turnPlayer}`).value
+    document.querySelector('h2').innerHTML = `${playerName} is the winner!`
 }
 
 function handleBoardClick(event) {
@@ -43,6 +81,18 @@ function handleBoardClick(event) {
     console.table(virtualBoard)
 
     disableRegion(span)
+
+    const winRegions = getWinRegions()
+    if (winRegions.length > 0) {
+        handleWin(winRegions)
+    }
+    else if (virtualBoard.flat().includes('')) {
+        turnPlayer = turnPlayer === 'player-one' ? 'player-two': 'player-one'
+        updateTurn()
+    }
+    else {
+        document.querySelector('h2').innerHTML = 'DRAW  '
+    }
 }
 
 document.querySelector('#start-game').addEventListener('click', initializeGame)

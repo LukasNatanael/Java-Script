511e8957451a09ebbe3f2914230dae576dd60993
Lukas Natanael <96913773+LukasNatanael@users.noreply.github.com>
2024-05-01 11:32:21 -0300

    common modules js

---
+++ onebitcode/java-script-modulo-4/aula-09-modulos-commonjs/index.js
@@ -0,0 +1,10 @@
+console.clear()
+
+const render = require('./render')
+const store = require('./store')
+
+console.log('Aplicação inicializada.')
+render()
+store()
+console.log('Aplicação finalizada.')
+console.log()

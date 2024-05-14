e66db6d17d243e61c2870e93aa4df3d04375f8a5
Lukas Natanael <96913773+LukasNatanael@users.noreply.github.com>
2024-05-14 07:39:54 -0300

    Update index.js

--- onebitcode/java-script-modulo-4/aula-14-npx-npm-scripts/index.js
+++ onebitcode/java-script-modulo-4/aula-14-npx-npm-scripts/index.js
@@ -1,2 +1,7 @@
+/*
+    npm -> serve para instalar módulos
+    npx -> serve executar códigos de módulos no terminal
+*/
+
 const HelloWorldNpm = require('hello-world-npm')
 console.log(`Executando hello-world-npm: ${HelloWorldNpm()}`)

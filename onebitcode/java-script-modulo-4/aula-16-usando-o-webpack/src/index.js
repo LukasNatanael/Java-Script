9327dd0a03c2ceeb56b97ed003970b018709373c
Lukas Natanael <96913773+LukasNatanael@users.noreply.github.com>
2024-05-15 15:03:20 -0300

    update plugins

--- onebitcode/java-script-modulo-4/aula-16-usando-o-webpack/src/index.js
+++ onebitcode/java-script-modulo-4/aula-16-usando-o-webpack/src/index.js
@@ -1,4 +1,6 @@
 import dayjs from 'dayjs'
 import './styles/index.css'
 
-alert(`Hoje é ${dayjs().format('DD/MM/YYYY')}`)
+// alert(`Hoje é ${dayjs().format('DD/MM/YYYY')}`)
+
+document.querySelector('h2').innerText = `Hoje é ${dayjs().format('DD/MM/YYYY')}`

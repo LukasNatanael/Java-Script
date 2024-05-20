91012280fb7aedff3c6a37e28ef28c347cd9cbe3
Lukas Natanael <96913773+LukasNatanael@users.noreply.github.com>
2024-05-20 09:18:29 -0300

    Revisando objetos
    Foi revisado o conceito de objetos e apresentado a `função construtora`, que funciona como uma classe.

---
+++ onebitcode/java-script-modulo-5/aula-01-revisando-objetos/index.js
@@ -0,0 +1,73 @@
+/*
+    Os objetos em JS permitem criar atributos de diversas formas sendo eles:
+        - Função comum:
+            function getSummary()
+        - Função anônima:
+            getTitle = () => { return this.title }
+        - Definindo após a criação do objeto:
+            book.save = () => { console.log('this function save the file in database') }
+*/
+const book = {
+    title: 'Harry Potter e a Pedra Filosofal',
+    author: {
+        name: 'J.K.Rowling'
+    },
+    publishYear: 1997,
+    pages: 317,
+    tags: ['fantasy', 'adventure', 'romance'],
+    inStock: 20,
+    getSummary() {
+        return `${this.title} foi escrito por ${this.author.name} em ${this.publishYear} com ${this.pages} páginas. Atualmente possuimos ${this.inStock} livros em estoque.`
+    },
+    addOnStock(quantity) {
+        this.inStock += quantity
+    }
+}
+
+console.clear()
+
+console.log(book.getSummary())
+console.log(book)
+console.log()
+
+book.addOnStock(50)
+console.log(book.getSummary())
+console.log()
+
+book.save = () => {
+    console.log('Livro salvo no banco de dados com sucesso!')
+}
+
+console.log(book)
+
+// Função CONSTRUTORA
+function Book(title, author, pages, publishYear, tags) {
+    this.title = title
+    this.author = author
+    this.pages = pages,
+    this.publishYear = publishYear,
+    this.tags = tags
+    this.published = false
+    this.inStock = 0
+    this.getSummary = function() {
+        return `${this.title} foi escrito por ${this.author.name} em ${this.publishYear} com ${this.pages} páginas.`
+    }
+    this.addOnStock = function(quantity) {
+        this.inStock += quantity
+    }
+    this.save = () => {
+        console.log('Livro salvo no banco de dados com sucesso!')
+    }
+}
+console.clear()
+const HarryPotterAuthor = { name: 'J.K.Rowling' }
+const HarryPotterTags = ['fantasy', 'adventure', 'romance']
+
+const HarryPotter = new Book('Harry Potter e a Pedra Filosofal', HarryPotterAuthor, 317, 1997, HarryPotterTags)
+console.log(HarryPotter.getSummary())
+
+const EldestAuthor = { name: 'Christopher Paolini' }
+const EldestTags = ['fantasy', 'adventure']
+
+const Eldest = new Book('Eldest', EldestAuthor, 468, 2005, EldestTags)
+console.log(Eldest.getSummary())

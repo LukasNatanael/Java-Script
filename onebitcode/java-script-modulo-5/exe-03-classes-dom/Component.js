9d4af5b91e9b22acb5c8d2699892f283e3747e00
Lukas Natanael <lukasnatanael889@gmail.com>
2024-05-28 10:08:10 -0300

    create sub class Input

--- onebitcode/java-script-modulo-5/exe-03-classes-dom/Component.js
+++ onebitcode/java-script-modulo-5/exe-03-classes-dom/Component.js
@@ -1,99 +1,108 @@
 class Component {
-    // #type
-    #elementStyle = document.createElement('style')
-    #element
-    constructor( type, width, height, background='white') {
+    // element
+    constructor(width, height, type='div') {
         // public properties
         this.type = type
         this.width = width
         this.heigth = height
-        this.background = background
+        // this.background = background
         
         // private properties
-        this.#element = document.createElement(this.type)
-        // this.#elementStyle = document.createElement('style')
-        this.#element.setAttribute('style', '')
+        this.element = document.createElement(this.type)
+        this.element.setAttribute('style', '')
+        this.element.setAttribute('class', this.type)
 
         // initialize component
-        this.#element.style.width = this.width
-        this.#element.style.height = this.heigth
-        this.#element.style.background = this.background
+        this.element.style.width = this.width
+        this.element.style.height = this.heigth
+        // this.#element.style.background = this.background
     }
 
     setStyleAll(style) {
-        this.#element.style = style
+        this.element.style = style
     }
 
     setID(id) {
         this.id = id
-        this.#element.setAttribute('id', this.id)
+        this.element.setAttribute('id', this.id)
     }
 
     setClass(classe) {
         this.class = classe
-        this.#element.setAttribute('class', this.class)
+        // this.#element.setAttribute('class', this.class)
+        this.element.classList.add(this.class)
     }
 
     setWidth(value) {
-        this.#element.style.width = value
+        this.element.style.width = value
     }
 
     setHeight(value) {
-        this.#element.style.height = value
+        this.element.style.height = value
     }
 
     setBackground(value) {
-        this.#element.style.background = value
+        this.element.style.background = value
     }
 
 
     getArea() {
-        return `${this.#element.style.width} X ${this.#element.style.height}`  
+        return `${this.element.style.width} X ${this.element.style.height}`  
     }
 
     getValue() {
-        return this.#element.value
+        return this.element.value
     }
 
     build() {
-        this.#element.style.display = 'none'
-        document.body.appendChild( this.#element )
+        this.element.style.display = 'none'
+        document.body.appendChild( this.element )
     }
     
     render() {
-        this.#element.style.display = 'block'
+        this.element.style.display = 'block'
     }
 }
 
 class Input extends Component {
-    #element
-    #elementStyle
     constructor(width, height) {
-        // super( width, height )
-        this.#element = document.createElement('input')
+        super(width, height, 'input')
 
-        console.log(`${width}`)
-
+        console.log(this.element)
     }
-    setPlaceholder(placeholder) {
-        this.#element.style.placeholder = placeholder
+
+    setPlaceholder(value) {
+        this.element.style.paddingLeft = '5px'
+        this.element.setAttribute('placeholder', value)
     }
 }
 
 console.clear()
-const div = new Component('div', '100px', '100px')
+const input = new Input()
 
-div.setStyleAll(`
-    height: 100px;
-    width: 100px;
-    background-color: red;
+input.setStyleAll(`
+    border: 1px solid aqua;
     border-radius: 10px;
+
+    display: flex;
+    flex-wrap: wrap;
+`)
+
+input.setID('name')
+input.setPlaceholder('Informe um nome')
+input.build()
+input.render()
+console.log(input)
+
+const div = new Component('100px', '100px')
+
+div.setClass('images')
+div.setStyleAll(`
+    border-radius: 10px;
+    border: 1px solid purple;
 `)
 
 div.build()
 div.render()
 
-
-const input = new Input('100px', '100px')
-input.build()
-input.render()
+console.log(div)

957c760e4dfb8e7cae9e4848c69327abd0963b8d
Lukas Natanael <lukasnatanael889@gmail.com>
2024-07-17 08:09:04 -0400

    update exe finanças

--- onebitcode/java-script-modulo-6/aula-14-requisições-post/index.js
+++ onebitcode/java-script-modulo-6/aula-14-requisições-post/index.js
@@ -3,7 +3,6 @@ function renderArticle(articleData) {
     const article = document.createElement('article')
     article.classList.add('article')
     article.id = `article-${articleData.id}`
-
     const title = document.createElement('h3')
     title.classList.add('article-title')
     title.textContent = articleData.title

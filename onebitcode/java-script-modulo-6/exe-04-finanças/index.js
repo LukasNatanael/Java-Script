40028895fecac6f6deb27f6f80ca5493d01b6d68
Lukas Natanael <lukasnatanael889@gmail.com>
2024-07-17 17:43:18 -0300

    update

--- onebitcode/java-script-modulo-6/exe-04-finanças/index.js
+++ onebitcode/java-script-modulo-6/exe-04-finanças/index.js
@@ -8,14 +8,20 @@ function renderArticle(articleData) {
     
     const itemTitle = document.createElement('strong')
     itemTitle.classList.add('item-title')
-    
     itemTitle.textContent = articleData.title
     
     const itemValue = document.createElement('p')
     itemValue.classList.add('item-value')
-    itemValue.textContent = articleData.value
+    // convertendo valor para real
+    // itemValue.textContent = `Valor: R$ ${articleData.value.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'})}`
+    itemValue.textContent = `Valor: R$ ${articleData.value}`
+
+    const itemSituation = document.createElement('p')
+    itemSituation.classList.add('item-situation')
+    itemSituation.textContent = `Situação: ${articleData.situation}`
+    itemSituation.value = articleData.situation
     
-    article.append(itemTitle, itemValue)
+    article.append(itemTitle, itemValue, itemSituation)
     document.querySelector('#articles').appendChild(article)
 
 }
@@ -28,13 +34,15 @@ async function fetchArticle() {
     response.forEach( (item) => console.log(item) )
 }
 
+// atualizando site ao carregar página
 document.addEventListener( 'DOMContentLoaded', () => {
     fetchArticle()
 } )
 
-const form = document.querySelector('form')
 
-form.addEventListener( 'submit', (event) => {
+// adicionando produtos ao database
+const form = document.querySelector('form')
+form.addEventListener( 'submit', async (event) => {
     event.preventDefault()
 
     // validando inputs
@@ -43,23 +51,55 @@ form.addEventListener( 'submit', (event) => {
 
     const price      = document.querySelector('#price')
     const priceError = document.querySelector('#price-error')
-    const situation  = document.querySelector('#situation')
+    const situation  = document.querySelector('input[name="situation"]:checked')
+    
+    // validando se o imput está vazio
+    try {
+
+        if ( product.value.length <= 0 ) {
+            productError.classList.add('error')
+            throw new Error('Por favor informe o nome do produto')
+        }
+        else {
+            productError.classList.remove('error')
+        }
+        
+        if ( price.value.length <= 0 ) {
+            priceError.classList.add('error')
+            throw new Error('Por favor informe o valor do produto')
+        }
+        else {
+            priceError.classList.remove('error')
+        }
+
+        const articleData = {
+            title: product.value,
+            value: price.value,
+            situation: situation.value
+        }
+        
+        form.reset()
+
+        console.log( articleData )
+
+        const response = await fetch('http://localhost:3000/products', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify( articleData )
+        })
+    
+        const savedArticle = await response.json()
+
+        console.log(savedArticle)
+
+        savedArticle.forEach( fetchArticle )
+    }
+    catch (error) {
+        console.log( error.message )
+    }
     
 
-    if ( product.value.length <= 0 ) {
-        productError.classList.add('error')
-    }
-    else {
-        productError.classList.remove('error')
-    }
-    
-    if ( price.value.length <= 0 ) {
-        priceError.classList.add('error')
-    }
-    else {
-        priceError.classList.remove('error')
-        
-    }
-    
 
 })
